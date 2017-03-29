c3bb971f1e573ac4286d241936f21dfdd76f88ab
Rofaeil Ashaiaa <RofaeilAshaiaa@gmail.com>
2017-03-29 16:09:23 +0200

    added Stock Detail fragment class and layout

---
+++ app/src/main/java/com/udacity/stockhawk/ui/StockDetailFargment.java
@@ -0,0 +1,27 @@
+package com.udacity.stockhawk.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.udacity.stockhawk.R;
+
+public class StockDetailFargment extends Fragment {
+
+
+    public StockDetailFargment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stock_detail, container, false);
+    }
+
+}
